ff7c1f07ed770d094d7cf0fc066fdc5638f65210
tonybear27 <tony20011027@gmail.com>
2024-01-14 18:48:57 +0800

    Upload

--- Roman to Integer.cpp
+++ Roman to Integer.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     int romanToInt(string s) {
         
-        int num = 0;
+        unordered_map<char, int> maps;
+
+        maps['M'] = 1000;
+        maps['D'] = 500;
+        maps['C'] = 100;
+        maps['L'] = 50;
+        maps['X'] = 10;
+        maps['V'] = 5;
+        maps['I'] = 1;
+
+        int ans = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if(s[i] == 'C' && s[i+1] == 'M') {
-                num += 900;
-                i++;
-            }
-            else if (s[i] == 'X' && s[i+1] == 'C') {
-                num += 90;
-                i++;
-            }
-            else if(s[i] == 'I' && s[i+1] == 'V') {
-                num += 4;
-                i++;
-            }
-            else if (s[i] == 'M') num += 1000;
-            else if (s[i] == 'D') num += 500;
-            else if (s[i] == 'C') num += 100;
-            else if (s[i] == 'L') num += 50;
-            else if (s[i] == 'X') num += 10;
-            else if (s[i] == 'V') num += 5;
-            else if (s[i] == 'I') num += 1;
+            if (maps[s[i]] < maps[s[i+1]])
+                ans -= maps[s[i]];
+            else
+                ans += maps[s[i]];
         }
 
-        return num;
+        return ans;
     }
 };
