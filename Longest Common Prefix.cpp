e90665b150796ec5b054bc964791e117508ddc44
tonybear27 <tony20011027@gmail.com>
2024-01-14 18:29:41 +0800

    Upload

---
+++ Longest Common Prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        string ans = "";
+
+        sort(strs.begin(), strs.end());
+
+        string first = strs[0], last = strs[strs.size() - 1];
+
+        for (int i = 0; i < min(first.length(), last.length()); i++) {
+            if (first[i] != last[i])
+                return ans;
+            
+            ans += first[i];
+        }
+
+        return ans;
+    }
+};
